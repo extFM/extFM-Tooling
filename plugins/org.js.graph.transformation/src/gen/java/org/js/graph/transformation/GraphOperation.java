9966ad75ed4779fd61eca484d9be9066b315c0dd
lucius <luciusapp@hotmail.com>
2013-05-24 21:15:17 +0200

    update gt model

---
+++ plugins/org.js.graph.transformation/src/gen/java/org/js/graph/transformation/GraphOperation.java
@@ -0,0 +1,17 @@
+/**
+ */
+package org.js.graph.transformation;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Graph Operation</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.js.graph.transformation.TransformationPackage#getGraphOperation()
+ * @model interface="true" abstract="true"
+ * @generated
+ */
+public interface GraphOperation extends Nameable {
+} // GraphOperation
