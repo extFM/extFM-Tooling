78978853389ac2041f0831e4c8225b413d95cc86
lucius <luciusapp@hotmail.com>
2013-06-13 14:41:12 +0200

    update rbac validation

--- plugins/org.js.model.rbac.validation/src/main/java/org/js/model/rbac/validation/UniqueDomainValueConstraint.java
+++ plugins/org.js.model.rbac.validation/src/main/java/org/js/model/rbac/validation/UniqueDomainValueConstraint.java
@@ -2,6 +2,8 @@ package org.js.model.rbac.validation;
 
 import org.eclipse.emf.common.util.EList;
 import org.js.model.rbac.AttributeDecision;
+import org.js.model.rbac.DeselectDomainValue;
+import org.js.model.rbac.SelectDomainValue;
 import org.js.model.rbac.SetAttribute;
 
 /**
@@ -10,29 +12,36 @@ import org.js.model.rbac.SetAttribute;
  */
 public class UniqueDomainValueConstraint extends AbstractAttributeConstraint {
 
-   /**
-    * Validate if an operation for the given domainvalue is already defined.
-    */
-   @Override
-   String checkAttributeConfiguration(SetAttribute attributeConfig, AttributeDecision operation) {
-      String errorMessage = null;
+	/**
+	 * Validate if an operation for the given domainvalue is already defined.
+	 */
+	@Override
+	String checkAttributeConfiguration(SetAttribute attributeConfig,
+			AttributeDecision operation) {
+		String errorMessage = null;
 
-      if (attributeConfig instanceof SetAttribute) {
-         SetAttribute setAttribute = (SetAttribute) attributeConfig;
+		if (attributeConfig instanceof SetAttribute) {
+			SetAttribute setAttribute = (SetAttribute) attributeConfig;
 
-         EList<AttributeDecision> valueOperations = setAttribute.getAttributeDecisions();
-         String newValue = operation.getValue();
-         for (AttributeDecision valueOperation : valueOperations) {
-            if (valueOperation != operation) {
-               String value = valueOperation.getValue();
-               if (value.equals(newValue)) {
-                  errorMessage = "A configuration operation on domain value '" + value + "' is already defined in this role.";
-                  break;
-               }
-            }
-         }
-      }
-      return errorMessage;
-   }
+			EList<AttributeDecision> valueOperations = setAttribute
+					.getAttributeDecisions();
+			String newValue = operation.getValue();
+			for (AttributeDecision valueOperation : valueOperations) {
+				if (valueOperation != operation) {
+					String value = valueOperation.getValue();
+					if (value.equals(newValue)) {
+						if ((operation instanceof SelectDomainValue && valueOperation instanceof SelectDomainValue)
+								|| (operation instanceof DeselectDomainValue && valueOperation instanceof DeselectDomainValue)) {
+							errorMessage = "A configuration operation on domain value '"
+									+ value
+									+ "' is already defined in this role.";
+							break;
+						}
+					}
+				}
+			}
+		}
+		return errorMessage;
+	}
 
 }
