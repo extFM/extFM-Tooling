f88481043da82807bc4bfcf3bee8d18cb5e76ed6
Julia Schroeter <julia.schroeter@sap.com>
2013-07-09 11:29:04 +0200

    comments added

--- plugins/org.js.model.feature/src/main/model/feature.cs
+++ plugins/org.js.model.feature/src/main/model/feature.cs
@@ -22,7 +22,6 @@ OPTIONS {
 }
 
 TOKENS {
-	//DEFINE INTEGER $('0')|(('1'..'9')('0'..'9')*)$;
 	DEFINE COMMENT $'//'(~('\n'|'\r'|'\uffff'))* $ ;
 }
 
@@ -40,26 +39,32 @@ TOKENSTYLES {
 }
 
 RULES {
+    // syntax definition for container class 'FeatureModel'
 	FeatureModel ::= "featuremodel" #1 name['"','"'] !0!0
 						domains* !0 root !0 constraints* ; 
 						
+    // syntax definition for features and their configuration state
 	@SuppressWarnings(explicitSyntaxChoice) 					
 	Feature ::= selected[selected : "selected", deselected : "deselected", unbound : ""] "feature" #1 name['"','"'] #1 "<" id[TEXT] ">"
 				(!1 (attributes | groups) )*; 
 	
+    // syntax definition for groups
 	Group ::= "group" #1 "<" id[TEXT] ">" #1 "(" minCardinality[TEXT] ".." maxCardinality[TEXT] ")" 
 						#1 "{" (!1 childFeatures)+ "}" !0;
+
+    // syntax definition for attributes and their configuration state
 	Attribute ::= name[TEXT] #1 "["  domain[] "]" ("\\" "{" deselectedDomainValues[TEXT] ("," #1 deselectedDomainValues[TEXT])* "}")? 
 	(#1 ":=" #1 (value['"','"']))? ;
 
+    // syntax definition for attribute domains
 	NumericalDomain ::= "domain" #1 "<" id[TEXT] ">" #1 "[" intervals ("," #1 intervals)* "]" !0;
 	Interval ::= lowerBound[TEXT] ".." upperBound[TEXT];
 
 	DiscreteDomain ::= "domain"  #1 "<" id[TEXT] ">" #1 "[" values ("," #1 values)* "]" !0;
-
 	DomainValue ::= (name[] "=")? #0 int[];
+
+    // syntax definition for cross-tree constraints
 	Imply ::= "constraint" #1 "<" id[TEXT] ">" #1 leftOperand[TEXT] #1 "->" #1 rightOperand[TEXT] !0;
-	
 	Exclude ::= "constraint" #1 "<" id[TEXT] ">" #1 leftOperand[TEXT] #1 "<->" #1 rightOperand[TEXT] !0;
 	
 	AttributeConstraint ::= "constraint" #1 "<" id[TEXT] ">" #1 
