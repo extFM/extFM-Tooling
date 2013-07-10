a24358076dc04cac0c286c972f9eb2c12edc7a44
Julia Schroeter <julia.schroeter@sap.com>
2013-07-10 22:53:13 +0200

    gitignore added and examples

--- plugins/org.js.model.rbac/src/main/model/rbac.cs
+++ plugins/org.js.model.rbac/src/main/model/rbac.cs
@@ -32,7 +32,7 @@ TOKENSTYLES {
 }	
 	
 	RULES {
-	// syntax definition for class 'StartMetaClass'
+	// syntax definition for container class 'AccessControlModel'
 	@SuppressWarnings(explicitSyntaxChoice) 
 	AccessControlModel   ::= "access control" #1 
 									("on" #1 featureModels['<','>'] ("," featureModels['<','>'])*) !0
@@ -40,30 +40,36 @@ TOKENSTYLES {
 									("permissions" #1 "{" !0 permissions ( ","!0 permissions)* "}")* !0
 									(roles | groups | subjects)* ; 
 	
+	// syntax definition for roles
 	@SuppressWarnings(nonContainmentOpposite) 
 	@SuppressWarnings(explicitSyntaxChoice) 
 	Role ::= "role" #1 name['"','"']? #1 id['<','>'] ("extends" (parentRoles[]) ("," parentRoles[])*)? !0 
 			(("{" (permissions['"','"'] | tasks)  ("," (permissions['"','"'] | tasks) )* "}") )? ;
 
-	// syntax definition for configuration operations
+	// syntax definition for feature configuration operations
 	SelectFeature ::= #4 "select" #1 feature[];
 	DeselectFeature ::= #4 "deselect" #1 feature[];
 	
+	// syntax definition for attribute configuration operations
 	SetAttribute ::= #4 "set" feature[] #0 "." #0 attribute[]
 						(#1 "{" #1 attributeDecisions ("," #1 attributeDecisions)* "}")* ;
 
 	SelectDomainValue ::= "+"#0 value[];
 	DeselectDomainValue ::= "-"#0 value[];
 	
-	ViewElement ::= "view" #1 resourceId[]; 
-	ModifyElement ::= "modify" #1 resourceId[];
 	
+	// syntax definition for subjects
 	@SuppressWarnings(nonContainmentOpposite) 
 	Subject ::= "subject" #1 name['"','"']? #1 id['<','>'] !0 
 			("{" roles[] ("," roles[])* "}")?;
 	
+	// syntax definition for groups
 	Group ::= "group" #1 name['"','"']? #1 id['<','>'] represents[] !0 
 			("{" contains[] ("," contains[])* "}")?;
 
+	// syntax definition for feature information model elements
 	Task ::= "task" name['"','"']? #1 id['<','>'];
+
+	ViewElement ::= "view" #1 resourceId[]; 
+	ModifyElement ::= "modify" #1 resourceId[];
 }
