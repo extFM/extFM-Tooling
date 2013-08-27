bd6237dd74d3b60db7371e17630122f160f1cbb6
David <David@davids-macbook-air.fritz.box>
2013-08-27 10:48:41 +0200

    ShowModelsContainingAFeature action implemented

---
+++ plugins/org.js.model.feature.quality.assurance/src/main/java/org/js/model/feature/quality/assurance/QAShowModelsContainingAFeature.java
@@ -0,0 +1,48 @@
+package org.js.model.feature.quality.assurance;
+
+import java.util.*;
+
+import org.eclipse.core.resources.IFile;
+import org.js.model.feature.*;
+import org.js.model.feature.csp.FeatureModelHelper;
+
+public class QAShowModelsContainingAFeature {
+	
+	private List<FeatureModel> configurations;
+	private Feature featureundertest;
+	
+	/**
+	 * Creates a new analyzer for identifying all configurations that
+	 * contain the selected Feature
+	 * @param files The list of configuration files
+	 * @param featureundertest The feature to search for
+	 */
+	public QAShowModelsContainingAFeature(List<IFile> files, Feature featureundertest) {
+		configurations = new ArrayList<FeatureModel>();
+		
+		// add feature models to configurations list 
+		for (IFile f : files) {
+			FeatureModel fm = FeatureModelHelper.getFeatureModel(f);
+			if(fm != null)
+				configurations.add(fm);
+		}
+		
+		this.featureundertest = featureundertest;
+	}
+	
+	/**
+	 * Identifies all configurations that contain the feature under test 
+	 * @return A set of configurations that contain the feature under test.
+	 */
+	public Set<FeatureModel> getAllModelsContainingTheFeature() {
+		Set<FeatureModel> results = new HashSet<FeatureModel>();
+		
+		for (FeatureModel currentModel : configurations) {
+			FeatureModelHelper currentModelHelper = new FeatureModelHelper(currentModel);
+			if(currentModelHelper.getFeature(featureundertest.getId().toString()) != null)
+				results.add(currentModel);
+		}
+		
+		return results;
+	}
+}
--- plugins/org.js.model.feature.quality.assurance/src/main/java/org/js/model/feature/quality/assurance/popup/actions/ShowModelsContainingAFeatureAction.java
+++ plugins/org.js.model.feature.quality.assurance/src/main/java/org/js/model/feature/quality/assurance/popup/actions/ShowModelsContainingAFeatureAction.java
@@ -2,15 +2,28 @@ package org.js.model.feature.quality.assurance.popup.actions;
 
 import java.util.*;
 
+import org.apache.log4j.Logger;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+import org.js.model.feature.Feature;
+import org.js.model.feature.FeatureModel;
+import org.js.model.feature.csp.FeatureModelHelper;
+import org.js.model.feature.quality.assurance.QAPluginHelper;
+import org.js.model.feature.quality.assurance.QAShowModelsContainingAFeature;
 
 public class ShowModelsContainingAFeatureAction implements IObjectActionDelegate {
 	
 	private Shell shell;
+	private Logger log = Logger.getLogger(ShowDeselectedFeaturesAction.class);
 	private ISelection currentSelection; 
 	
 	/**
@@ -33,8 +46,78 @@ public class ShowModelsContainingAFeatureAction implements IObjectActionDelegate
 	 * @see IActionDelegate#run(IAction)
 	 */
 	public void run(IAction action) {
-		// TODO: Implement this!
+		// jump out test
+		if(currentSelection == null || currentSelection.isEmpty()) {
+			MessageDialog.openError(
+					shell, 
+					"Quality Assurance", 
+					"There is no folder selected. Please select a folder and then execute this command.");
+			return;
+		}
 		
+		// get files
+		List<IFile> files = QAPluginHelper.getFilesFromFolderSelection(currentSelection);
+		if(files == null) {
+			MessageDialog.openError(shell, "Quality Assurance", "An error occured during selection retrieval.");
+			log.debug("An error occured during selection retrieval.");
+			return;
+		}
+		
+		// get feature under test
+		
+		// - take a model
+		FeatureModel model = null;
+		for (IFile f : files) {
+			FeatureModel fm = FeatureModelHelper.getFeatureModel(f);
+			if(fm != null) {
+				model = fm;
+				break;
+			}
+		}
+		if(model == null) {
+			MessageDialog.openError(shell, "Quality Assurance", "No model found in this folder.");
+			log.debug("No model found in this folder.");
+			return;
+		}
+		// - extract all features
+		FeatureModelHelper modelhelper = new FeatureModelHelper(model);
+		Set<Feature> modelfeatures = modelhelper.getAllFeatures();
+		List<String> modelfeatures_ids = new ArrayList<String>();
+		for (Feature f : modelfeatures) {
+			modelfeatures_ids.add(f.getId().toString());
+		}
+		ElementListSelectionDialog selectFeatureDialog = new ElementListSelectionDialog(shell, new LabelProvider());
+		selectFeatureDialog.setElements(modelfeatures_ids.toArray());
+		selectFeatureDialog.setTitle("Quality Assurance");
+		selectFeatureDialog.setMessage("Please select the desired feature (ID) under test.");
+		selectFeatureDialog.setMultipleSelection(false);
+		if (selectFeatureDialog.open() != Window.OK) {
+			log.debug("No feature selected.");
+		    return;
+		}
+		String result = (String)(selectFeatureDialog.getResult()[0]);
+		log.info("The feature " + result + " will be tested.");
+		Feature featureundertest = modelhelper.getFeature(result);
+		
+		// run algorithm
+		QAShowModelsContainingAFeature analyzer = new QAShowModelsContainingAFeature(files, featureundertest);
+		Set<FeatureModel> results = analyzer.getAllModelsContainingTheFeature();
+		
+		// generate output
+		log.info("===================================================================");
+		log.info("Quality Assurance - Plugin - ShowModelsContainingAFeatureAction");
+		log.info("===================================================================");
+		log.info("Number of configurations found: " + results.size());
+		log.info("The following models can be indentifyed to contain " + featureundertest.getId() + ":");
+		for (FeatureModel resultsmodel : results) {
+			log.info("  * " + resultsmodel.getName());
+		}
+		log.info("===================================================================");
+		
+		MessageDialog.openInformation(shell, 
+				"Quality Assurance", 
+				"There are " + results.size() + " configurations that contain the feature " + featureundertest.getId() + ". " +
+				"For further information, please have a look at the logger output.");
 	}
 
 	/**
