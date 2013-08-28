4c28c508f496b34fcee7eb56c797a035e3a9957f
David <David@davids-macbook-air.fritz.box>
2013-08-28 21:05:37 +0200

    Start implementation of QA ShowFeatureDependencies

---
+++ plugins/org.js.model.feature.quality.assurance/src/main/java/org/js/model/feature/quality/assurance/QAShowFeatureDependencies.java
@@ -0,0 +1,83 @@
+package org.js.model.feature.quality.assurance;
+
+import java.util.*;
+
+import org.eclipse.core.resources.*;
+import org.js.model.feature.*;
+import org.js.model.feature.csp.FeatureModelHelper;
+
+public class QAShowFeatureDependencies {
+	private List<FeatureModel> configurations;
+	private FeatureModel model;
+	private Feature featureundertest;
+	
+	/**
+	 * Creates a new analyzer for identifying all configurations that
+	 * contain the selected Feature
+	 * @param files The list of configuration files
+	 * @param featureundertest The feature to search for
+	 */
+	public QAShowFeatureDependencies(List<IFile> files, Feature featureundertest) {
+		configurations = new ArrayList<FeatureModel>();
+		
+		// add feature models to configurations list 
+		for (IFile f : files) {
+			FeatureModel fm = FeatureModelHelper.getFeatureModel(f);
+			if(fm != null)
+				configurations.add(fm);
+		}
+		
+		// select one model for analzsis
+		model = configurations.get(0);
+		
+		this.featureundertest = featureundertest;
+	}
+	
+	private boolean isFeatureMandatory() throws Exception {
+		// a feature is mandatory if the parent group requires the selection
+		// of at least the amount of features that this group contains.
+		
+		boolean isMandatory = true;	// per default, a feature is mandatory (e.g. root feature)
+		
+		// select the concrete feature
+		Feature feature = null;
+		FeatureModelHelper modelHelper = new FeatureModelHelper(model);
+		feature = modelHelper.getFeature(featureundertest.getId().toString());
+		
+		if(feature == null) throw new java.lang.Exception("The feature is not part of the model.");
+		
+		// is this featureundertest the root? ...else look up in groups
+		if(model.getRoot() == feature) {
+			isMandatory = true;
+			return isMandatory;
+		}
+		
+		// find group this feature is part of
+		boolean readyToBreak = false;
+		Set<Feature> allFeatures = modelHelper.getAllFeatures();
+		for (Feature f : allFeatures) {
+			Set<Group> allGroupsOfFeature = new HashSet<Group>();
+			allGroupsOfFeature.addAll(f.getGroups());
+			for (Group g : allGroupsOfFeature) {
+				Set<Feature> allFeaturesOfGroup = new HashSet<Feature>();
+				allFeaturesOfGroup.addAll(g.getChildFeatures());
+				
+				for (Feature f2 : allFeaturesOfGroup) {
+					if(f2 == feature) {
+						readyToBreak = true;
+						if(allFeaturesOfGroup.size() == g.getMinCardinality()) {
+							isMandatory = true;
+						}
+						break;
+					}
+				}
+				
+				if(readyToBreak) break;
+			}
+			
+			if(readyToBreak) break;
+		}
+		
+		return isMandatory;
+	}
+}
--- plugins/org.js.model.feature.quality.assurance/src/main/java/org/js/model/feature/quality/assurance/popup/actions/ShowFeatureDependenciesAction.java
+++ plugins/org.js.model.feature.quality.assurance/src/main/java/org/js/model/feature/quality/assurance/popup/actions/ShowFeatureDependenciesAction.java
@@ -2,18 +2,28 @@ package org.js.model.feature.quality.assurance.popup.actions;
 
 import java.util.*;
 
+import org.apache.log4j.Logger;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IObjectActionDelegate;
 import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+import org.js.model.feature.Feature;
+import org.js.model.feature.FeatureModel;
+import org.js.model.feature.csp.FeatureModelHelper;
 import org.js.model.feature.quality.assurance.QAPluginHelper;
+import org.js.model.feature.quality.assurance.QAShowFeatureDependencies;
+import org.js.model.feature.quality.assurance.QAShowModelsContainingAFeature;
 
 public class ShowFeatureDependenciesAction implements IObjectActionDelegate {
 	
 	private Shell shell;
+	private Logger log = Logger.getLogger(ShowDeselectedFeaturesAction.class);
 	private ISelection currentSelection; 
 	
 	/**
@@ -47,15 +57,70 @@ public class ShowFeatureDependenciesAction implements IObjectActionDelegate {
 		
 		// get files
 		List<IFile> files = QAPluginHelper.getFilesFromFolderSelection(currentSelection);
+		if(files == null) {
+			MessageDialog.openError(shell, "Quality Assurance", "An error occured during selection retrieval.");
+			log.debug("An error occured during selection retrieval.");
+			return;
+		}
 		
 		// get feature under test
-		//TODO implementation
+		
+		// - take a model
+		FeatureModel model = null;
+		for (IFile f : files) {
+			FeatureModel fm = FeatureModelHelper.getFeatureModel(f);
+			if(fm != null) {
+				model = fm;
+				break;
+			}
+		}
+		if(model == null) {
+			MessageDialog.openError(shell, "Quality Assurance", "No model found in this folder.");
+			log.debug("No model found in this folder.");
+			return;
+		}
+		// - extract all features
+		FeatureModelHelper modelhelper = new FeatureModelHelper(model);
+		Set<Feature> modelfeatures = modelhelper.getAllFeatures();
+		List<String> modelfeatures_ids = new ArrayList<String>();
+		for (Feature f : modelfeatures) {
+			modelfeatures_ids.add(f.getId().toString());
+		}
+		ElementListSelectionDialog selectFeatureDialog = new ElementListSelectionDialog(shell, new LabelProvider());
+		selectFeatureDialog.setElements(modelfeatures_ids.toArray());
+		selectFeatureDialog.setTitle("Quality Assurance");
+		selectFeatureDialog.setMessage("Please select the desired feature (ID) under test.");
+		selectFeatureDialog.setMultipleSelection(false);
+		if (selectFeatureDialog.open() != Window.OK) {
+			log.debug("No feature selected.");
+		    return;
+		}
+		String result = (String)(selectFeatureDialog.getResult()[0]);
+		log.info("The feature " + result + " will be tested.");
+		Feature featureundertest = modelhelper.getFeature(result);
 		
 		// run algorithm
-		//TODO implementation
+		//TODO: extend this part and remove old code afterwards
+		QAShowFeatureDependencies analyzer2 = new QAShowFeatureDependencies(files, featureundertest);
+		
+		QAShowModelsContainingAFeature analyzer = new QAShowModelsContainingAFeature(files, featureundertest);
+		Set<FeatureModel> results = analyzer.getAllModelsContainingTheFeature();
 		
 		// generate output
-		//TODO implementation
+		log.info("===================================================================");
+		log.info("Quality Assurance - Plugin - ShowFeatureDependenciesAction");
+		log.info("===================================================================");
+		log.info("Number of configurations found: " + results.size());
+		log.info("The following models can be indentifyed to contain " + featureundertest.getId() + ":");
+		for (FeatureModel resultsmodel : results) {
+			log.info("  * " + resultsmodel.getName());
+		}
+		log.info("===================================================================");
+		
+		MessageDialog.openInformation(shell, 
+				"Quality Assurance", 
+				"There are " + results.size() + " configurations that contain the feature " + featureundertest.getId() + ". " +
+				"For further information, please have a look at the logger output.");
 	}
 
 	/**
