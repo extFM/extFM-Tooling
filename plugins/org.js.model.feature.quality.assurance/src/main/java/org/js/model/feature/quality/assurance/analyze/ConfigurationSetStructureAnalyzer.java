5a1536f02d8de947eb3af29e3e8ffdaaae21fc11
David <David@davids-macbook-air.fritz.box>
2013-09-06 14:20:35 +0200

    StructureAnalyzer updated and ConfigurationSetStructureAnalyzer implemented

--- plugins/org.js.model.feature.quality.assurance/src/main/java/org/js/model/feature/quality/assurance/analyze/ConfigurationSetStructureAnalyzer.java
+++ plugins/org.js.model.feature.quality.assurance/src/main/java/org/js/model/feature/quality/assurance/analyze/ConfigurationSetStructureAnalyzer.java
@@ -1,9 +1,59 @@
 package org.js.model.feature.quality.assurance.analyze;
 
+import java.util.*;
+
+import org.js.model.feature.*;
+import org.js.model.feature.csp.FeatureModelHelper;
+
 public class ConfigurationSetStructureAnalyzer extends StructureAnalyzer {
 
 	public ConfigurationSetStructureAnalyzer() {
-		// TODO Auto-generated constructor stub
+		super();
+	}
+	
+	public ConfigurationSetStructureAnalyzer(Set<FeatureModel> models) {
+		super(models);
+	}
+	
+	/**
+	 * Identify all constantly deselected features in all models under test. 
+	 * Ensure that all configurations are based on the same feature model!
+	 * @return A set of all features constantly deselected 
+	 */
+	public Set<Feature> getDeselectedFeatures() {
+		//get a list with all features
+		if(!isModelsSet()) return null;
+		if(!setModel((FeatureModel)(getModels().toArray()[0]))) return null;
+		FeatureModelHelper helper = new FeatureModelHelper(getModel());
+		Set<Feature> features = helper.getAllFeatures();
+		
+		// iterate over all configurations
+		for (FeatureModel model : getModels()) {
+			FeatureModelHelper modelHelper = new FeatureModelHelper(model);
+			
+			// collect all features to potentially remove from results set
+			List<Feature> toDelete = new ArrayList<Feature>();
+			toDelete.addAll(modelHelper.getSelectedFeatures());
+			toDelete.addAll(modelHelper.getUnboundFeatures());
+			
+			// iterate over all features-to-be-deleted and remove them from the results set if contained
+			for (Feature f : toDelete) {
+				Feature ref = null;
+				
+				for (Feature g : features) {
+					if(g.getId() == f.getId()) {
+						ref = g;
+						break;
+					}
+				}
+				
+				if(ref != null)
+					features.remove(ref);
+			}
+		}
+		
+		// return results set
+		return features;
 	}
 
 }
--- plugins/org.js.model.feature.quality.assurance/src/main/java/org/js/model/feature/quality/assurance/analyze/StructureAnalyzer.java
+++ plugins/org.js.model.feature.quality.assurance/src/main/java/org/js/model/feature/quality/assurance/analyze/StructureAnalyzer.java
@@ -7,10 +7,32 @@ import org.js.model.feature.csp.FeatureModelHelper;
 
 public abstract class StructureAnalyzer {
 	
+	/**
+	 * Representation of the (set of) configurations/models under test
+	 */
 	private Set<FeatureModel> models;
+	/**
+	 * Representation of the configuration/model under test
+	 */
 	private FeatureModel model;
+	/**
+	 * Representation of the feature under test
+	 */
 	private Feature feature;
 	
+	/**
+	 * Creates a full new Structure Analyzer with all input data (models, a model under test and a feature under test).
+	 * <p>Please pay attention that the model must be contained in the models set and the feature must be one of
+	 * the model. If not, the object cannot be created.</p>
+	 * <p>Take as well into account that this method assumes that
+	 * all models are of one and the same base feature model. There is no explicit test for this. If there are models
+	 * that are based on a different feature model, this may cause unexpected behavior.</p>
+	 * 
+	 * @param models A set of (staged) configurations
+	 * @param model One specific model that must be contained in the models set
+	 * @param feature One specific feature that must be one of the model
+	 * @throws Exception Throws if the model is not part of the models set or if the feature is not one of the model
+	 */
 	public StructureAnalyzer(Set<FeatureModel> models, FeatureModel model, Feature feature) throws Exception {
 		setModels(models);
 		if(!setModel(model))
@@ -19,26 +41,61 @@ public abstract class StructureAnalyzer {
 			throw new Exception("The feature could not be set.");
 	}
 	
+	/**
+	 * Creates a new Structure Analyzer with models and a model as input data.
+	 * <p>Please pay attention that the model must be contained in the models set. If not, the object cannot be created.</p>
+	 * <p>Take as well into account that this method assumes that
+	 * all models are of one and the same base feature model. There is no explicit test for this. If there are models
+	 * that are based on a different feature model, this may cause unexpected behavior.</p>
+	 * @param models A set of (staged) configurations
+	 * @param model model One specific model that must be contained in the models set
+	 * @throws Exception Throws if the model is not part of the models set
+	 */
 	public StructureAnalyzer(Set<FeatureModel> models, FeatureModel model) throws Exception {
 		setModels(models);
 		if(!setModel(model))
 			throw new Exception("The model could not be set.");
 	}
 	
+	/**
+	 * Creates a new Structure Analyzer with models as input data.
+	 * <p>Take into account that this method assumes that
+	 * all models are of one and the same base feature model. There is no explicit test for this. If there are models
+	 * that are based on a different feature model, this may cause unexpected behavior.</p>
+	 * @param models A set of (staged) configurations
+	 */
 	public StructureAnalyzer(Set<FeatureModel> models) {
 		setModels(models);
 	}
 	
+	/**
+	 * Creates a new Structure Analyzer without any input data.
+	 */
 	public StructureAnalyzer() {}
 	
+	/**
+	 * Sets or resets the models set. If this invalidates the model and/or feature, they will be reseted to null
+	 * @param models
+	 */
 	protected void setModels(Set<FeatureModel> models) {
 		this.models = models;
+		
+		if(isModelSet() && !setModel(model)) model = null;
+		if(isFeatureSet() && !setFeature(feature)) feature = null;
 	}
 	
+	/**
+	 * Returns the models set if existing (else null)
+	 * @return
+	 */
 	protected Set<FeatureModel> getModels() {
 		return this.models;
 	}
 	
+	/**
+	 * Checks if the models set is set
+	 * @return <code>True</code> if the models set is not null, else <code>False</code>.
+	 */
 	protected boolean isModelsSet() {
 		if(this.models != null)
 			return true;
@@ -46,18 +103,34 @@ public abstract class StructureAnalyzer {
 			return false;
 	}
 	
+	/**
+	 * Sets or resets the model. This must be part of the models set. If this invalidates the feature, this will be reseted to null
+	 * @param model
+	 * @return <code>True</code> if the model has been set. <code>False</code> if the model is invalid.
+	 */
 	protected boolean setModel(FeatureModel model) {
-		if(!models.contains(model))
+		if(!isModelsSet() || !models.contains(model))
 			return false;
 		
 		this.model = model;
+		
+		if(isFeatureSet() && !setFeature(feature)) feature = null;
+		
 		return true;
 	}
 	
+	/**
+	 * Returns the model if existing (else null)
+	 * @return
+	 */
 	protected FeatureModel getModel() {
 		return this.model;
 	}
 	
+	/**
+	 * Checks if the model is set
+	 * @return <code>True</code> if the model is not null, else <code>False</code>.
+	 */
 	protected boolean isModelSet() {
 		if(this.model != null)
 			return true;
@@ -65,7 +138,13 @@ public abstract class StructureAnalyzer {
 			return false;
 	}
 	
+	/**
+	 * Sets or resets the feature. This must be one of the model.
+	 * @param feature
+	 * @return <code>True</code> if the feature has been set. <code>False</code> if the feature is invalid.
+	 */
 	protected boolean setFeature(Feature feature) {
+		if(!isModelSet()) return false;
 		FeatureModelHelper modelHelper = new FeatureModelHelper(model);
 		Set<Feature> features = modelHelper.getAllFeatures();
 		if(!features.contains(feature))
@@ -75,14 +154,23 @@ public abstract class StructureAnalyzer {
 		return true;
 	}
 	
+	/**
+	 * Returns the feature if existing (else null)
+	 * @return
+	 */
 	protected Feature getFeature() {
 		return this.feature;
 	}
 	
+	/**
+	 * Checks if the feature is set
+	 * @return <code>True</code> if the feature is not null, else <code>False</code>.
+	 */
 	protected boolean isFeatureSet() {
 		if(this.feature != null)
 			return true;
 		else
 			return false;
 	}
+	
 }
