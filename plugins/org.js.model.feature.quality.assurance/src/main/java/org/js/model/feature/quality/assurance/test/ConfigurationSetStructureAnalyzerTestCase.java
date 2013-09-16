5bff4287c4ff106eefb32aa32c820f2b2d74e0e9
David <David@davids-macbook-air.fritz.box>
2013-09-16 12:39:39 +0200

    TestCases applied (not yet filled with data, only structure prepared)

---
+++ plugins/org.js.model.feature.quality.assurance/src/main/java/org/js/model/feature/quality/assurance/test/ConfigurationSetStructureAnalyzerTestCase.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+package org.js.model.feature.quality.assurance.test;
+
+import junit.framework.TestCase;
+
+/**
+ * @author David
+ *
+ */
+public class ConfigurationSetStructureAnalyzerTestCase extends TestCase {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	protected void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link org.js.model.feature.quality.assurance.analyze.ConfigurationSetStructureAnalyzer#ConfigurationSetStructureAnalyzer(java.util.Set)}.
+	 */
+	public void testConfigurationSetStructureAnalyzer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.js.model.feature.quality.assurance.analyze.ConfigurationSetStructureAnalyzer#resetModels(java.util.Set)}.
+	 */
+	public void testResetModels() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.js.model.feature.quality.assurance.analyze.ConfigurationSetStructureAnalyzer#getDeselectedFeatures()}.
+	 */
+	public void testGetDeselectedFeatures() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ plugins/org.js.model.feature.quality.assurance/src/main/java/org/js/model/feature/quality/assurance/test/FeatureBasedStructureAnalyzerTestCase.java
@@ -0,0 +1,98 @@
+/**
+ * 
+ */
+package org.js.model.feature.quality.assurance.test;
+
+import junit.framework.TestCase;
+
+/**
+ * @author David
+ *
+ */
+public class FeatureBasedStructureAnalyzerTestCase extends TestCase {
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/**
+	 * Test method for {@link org.js.model.feature.quality.assurance.analyze.FeatureBasedStructureAnalyzer#FeatureBasedStructureAnalyzer(java.util.Set, org.js.model.feature.FeatureModel, org.js.model.feature.Feature)}.
+	 */
+	public void testFeatureBasedStructureAnalyzer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.js.model.feature.quality.assurance.analyze.FeatureBasedStructureAnalyzer#resetAll(java.util.Set, org.js.model.feature.FeatureModel, org.js.model.feature.Feature)}.
+	 */
+	public void testResetAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.js.model.feature.quality.assurance.analyze.FeatureBasedStructureAnalyzer#resetFeature(org.js.model.feature.Feature)}.
+	 */
+	public void testResetFeature() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.js.model.feature.quality.assurance.analyze.FeatureBasedStructureAnalyzer#getFeatureModelsContainingFeature()}.
+	 */
+	public void testGetFeatureModelsContainingFeature() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.js.model.feature.quality.assurance.analyze.FeatureBasedStructureAnalyzer#isFeatureMandatory()}.
+	 */
+	public void testIsFeatureMandatory() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.js.model.feature.quality.assurance.analyze.FeatureBasedStructureAnalyzer#getFeatureConstraintsAffectingFeature()}.
+	 */
+	public void testGetFeatureConstraintsAffectingFeature() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.js.model.feature.quality.assurance.analyze.FeatureBasedStructureAnalyzer#getFeatureConstraintsAffectingFeatureInAnyOperand()}.
+	 */
+	public void testGetFeatureConstraintsAffectingFeatureInAnyOperand() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.js.model.feature.quality.assurance.analyze.FeatureBasedStructureAnalyzer#getFeatureConstraintsAffectingFeatureInImplyRightOperand()}.
+	 */
+	public void testGetFeatureConstraintsAffectingFeatureInImplyRightOperand() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.js.model.feature.quality.assurance.analyze.FeatureBasedStructureAnalyzer#getFeatureConstraintsAffectingFeatureInImplyLeftOperand()}.
+	 */
+	public void testGetFeatureConstraintsAffectingFeatureInImplyLeftOperand() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.js.model.feature.quality.assurance.analyze.FeatureBasedStructureAnalyzer#getFeatureConstraintsAffectingFeatureInExcludeRightOperand()}.
+	 */
+	public void testGetFeatureConstraintsAffectingFeatureInExcludeRightOperand() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link org.js.model.feature.quality.assurance.analyze.FeatureBasedStructureAnalyzer#getFeatureConstraintsAffectingFeatureInExcludeLeftOperand()}.
+	 */
+	public void testGetFeatureConstraintsAffectingFeatureInExcludeLeftOperand() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
